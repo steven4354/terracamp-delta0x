07148e384c1530303946f1cc4bc0fbb5eadcbe1b
Steven Li <steven4354@gmail.com>
2021-08-31 14:34:30 -0700

    testing with mainnet

--- deploy/scripts/mirror_test.ts
+++ deploy/scripts/mirror_test.ts
@@ -13,35 +13,42 @@ import { Mirror } from "@mirror-protocol/mirror.js";
     });
 
     // const wallet = terra.wallet(mk);
+    // {
+    //     // lcd: terra,
+    //     // key: mk,
+    //     //   mint: "terra1s9ehcjv0dqj2gsl72xrpp0ga5fql7fj7y3kq3w"
+    //   }
     const mirror = new Mirror({
-      lcd: terra,
-      key: mk,
-      mint: "terra1s9ehcjv0dqj2gsl72xrpp0ga5fql7fj7y3kq3w",
+        key: mk
     });
 
-    const wallet = mirror.lcd?.wallet(mirror.key);
+    const result = await mirror.factory.getConfig()
+    console.log("STEVENDEBUG result ", result);
+    
+    // const wallet = mirror.lcd?.wallet(mirror.key);
+    const wallet = terra.wallet(mk);
 
     const positionIdx = await mirror.mint.getNextPositionIdx();
     // cosnt asset = await mirror.mint.getAssetConfig('terra18yx7ff8knc98p07pdkhm3u36wufaeacv47fuha')
 
     console.log("STEVENDEBUG positionIdx ", positionIdx);
-    const austContractAddr = "terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl";
-    const mQQQContractAddr = "terra12s2h8vlztjwu440khpc0063p34vm7nhu25w4p9";
+    const austContractAddrTestnet = "terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl";
+    const mQQQContractAddrTestnet = "terra12s2h8vlztjwu440khpc0063p34vm7nhu25w4p9";
+    const mTSLAContractAddrMainnet = "terra14y5affaarufk3uscy2vr6pe6w6zqf2wpjzn5sh";
 
     const deposit = mirror.mint.deposit(positionIdx.next_position_idx, {
       info: {
-        // token: {
-        //   contract_addr: austContractAddr,
-        // },
-        native_token: {
-            denom: 'uusd'
-        }
+        token: {
+          contract_addr: mTSLAContractAddrMainnet,
+        },
+        // native_token: {
+        //     denom: 'uusd'
+        // }
       },
-      amount: "100",
+      amount: "1",
     });
 
     console.log("STEVENDEBUG deposit ", deposit);
-
 
     const depositTx = await wallet?.createAndSignTx({
         msgs: [deposit],
