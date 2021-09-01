2517d7c82d9c4f85dc54594ad208849ef718ff05
Steven Li <steven4354@gmail.com>
2021-08-31 20:53:47 -0700

    using mETH

--- deploy/scripts/mirror_test.ts
+++ deploy/scripts/mirror_test.ts
@@ -40,6 +40,7 @@ import { Mirror } from "@mirror-protocol/mirror.js";
     const mQQQContractAddrTestnet = "terra12s2h8vlztjwu440khpc0063p34vm7nhu25w4p9";
     const mTSLAContractAddrMainnet = "terra14y5affaarufk3uscy2vr6pe6w6zqf2wpjzn5sh";
     const mSLVContractAddrMainnet = "terra1kscs6uhrqwy6rx5kuw5lwpuqvm3t6j2d6uf2lp";
+    const mEthContractAddrMainnet = "terra1dk3g53js3034x4v5c3vavhj2738une880yu6kx";
 
     const openPositionCollateral = {
       info: {
@@ -50,17 +51,18 @@ import { Mirror } from "@mirror-protocol/mirror.js";
             denom: 'uusd'
         }
       },
-      amount: int`5`.toString()
+      // 1 UST
+      amount: int`1000000`.toString()
     }
     const openPosition = await mirror.mint.openPosition(
       openPositionCollateral,
       {
         token: {
-          contract_addr: mSLVContractAddrMainnet,
+          contract_addr: mEthContractAddrMainnet,
         },
       },
       // openPositionCollateral.info,
-      1.5,
+      2.1,
     )
     console.log("STEVENDEBUG openPosition ", JSON.stringify(openPosition));
     const openPositionTx = await wallet?.createAndSignTx({
